044b36fe5eaacf6efd98ab1aecace1cb7287e71e
Anders Refsdal Olsen <anders@refsdal.io>
2025-08-08 21:39:10 +0200

    chore: add save the date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 import Head from 'next/head';
 
 export default function Home() {
+    const date = "26. September 2026";
+    
     return (
-        <div>
-            <Head>
-                <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&display=swap" rel="stylesheet" />
-            </Head>
-
-            {/* Setting the full height with black background */}
-            <div className="flex items-center justify-center min-h-screen bg-black flex-col">
-                <h1 className="text-8xl font-bold font-serif text-white mb-6">Hun sa ja!</h1>
-                <p className="text-xl text-white">Mer informasjon kommer snart...</p>
-            </div>
-        </div>
+      <div>
+          <Head>
+              <link rel="preconnect" href="https://fonts.googleapis.com" />
+              <link rel="preconnect" href="https://fonts.gstatic.com" />
+              <link href="https://fonts.googleapis.com/css2?family=Alex+Brush&display=swap" rel="stylesheet" />
+          </Head>
+          
+          <div className="border-3 border-white h-[400px] w-[400px] mx-auto mt-20 p-10 flex flex-col items-center justify-center rounded-full darker-green">
+              <h1 className="text-6xl text-white alex-brush-regular mb-6">Vi gifter oss!</h1>
+              <p className="text-xl text-white alex-brush-regular">
+                  Hold av lørdag <strong>{date}</strong>!
+              </p>
+          </div>
+      </div>
     );
 }
